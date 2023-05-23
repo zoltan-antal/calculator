3da35bf7ba1d100ebeca948eb659c398c0df7449
zoltan-antal <zoltan_antal@outlook.com>
2023-05-23 15:35:11 +0200

    Add operate() function

--- script.js
+++ script.js
@@ -2,18 +2,35 @@ let firstNumber;
 let secondNumber;
 let operation;
 
-function add (a, b) {
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+      break;
+    case "-":
+      return subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      return multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      return divide(firstNumber, secondNumber);
+      break;
+  }
+}
+
+function add(a, b) {
   return a + b;
 }
 
-function subtract (a, b) {
+function subtract(a, b) {
   return a - b;
 }
 
-function multiply (a, b) {
+function multiply(a, b) {
   return a * b;
 }
 
-function divide (a, b) {
+function divide(a, b) {
   return Math.round((a / b) * 1000) / 1000;
 }
